21961e6c5c5a5dbcdba238eca7cdd99b2ec897c9
Taewoo <confort28@gmail.com>
2024-09-19 18:50:26 +0900

    chatbot service update

--- src/main/java/org/big18/finale/config/ServerConfiguration.java
+++ src/main/java/org/big18/finale/config/ServerConfiguration.java
@@ -1,7 +1,7 @@
 package org.big18.finale.config;
 
 public interface ServerConfiguration {
-    final String REDISHOST = "172.20.160.1";
+    final String REDISHOST = "redis_rocky";
     final String REDISPORT = "6379";
-    final String APIURL = "http://172.20.160.1:8001";
+    final String APIURL = "http://172.20.0.5:8000";
 }
