51a41146910208a904369242676146488fce4cf5
Taewoo <confort28@gmail.com>
2024-09-25 11:41:17 +0900

    docker file updates

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM    openjdk:17-jdk
 LABEL   authors="Taewoo-HN"
 
-COPY    build/libs/JERONIMO-0.0.1-TEST.jar app.jar
+COPY    build/libs/*.jar app.jar
 
 ENTRYPOINT ["java", "-jar", "/app.jar"]
--- run_conda.sh
+++ run_conda.sh
@@ -1,12 +1,13 @@
+#!/bin/bash
 # Conda 환경 활성화
 . /root/anaconda3/etc/profile.d/conda.sh
 conda activate ml-dev
 
 systemctl start kafka
 
-airflow celery worker
-airflow scheduler
-airflow webserver
+nohup airflow celery worker
+nohup airflow scheduler
+nohup airflow webserver
 
 
 
