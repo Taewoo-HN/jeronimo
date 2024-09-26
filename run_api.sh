e16d1f679fdb8e651351189e70427bff6022cd04
Taewoo <confort28@gmail.com>
2024-09-26 17:21:45 +0900

    GCP deploy

--- run_api.sh
+++ run_api.sh
@@ -1,4 +1,4 @@
-. /root/anaconda3/etc/profile.d/conda.sh
-cd /usr/local/etc/ModelAPI || exit 1
-conda activate tensorflow_cuda
+. /root/miniconda3/etc/profile.d/conda.sh
+cd /root/serving/ModelAPI || exit 1
+conda activate cpu_ai
 uvicorn main:app --host=0.0.0.0
