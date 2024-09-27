cde4d8274c9b2a1a150ae7807bc9f26b8a514e61
Taewoo <confort28@gmail.com>
2024-09-27 13:51:20 +0900

    response formatting change

--- src/main/resources/static/js/chatbot_request.js
+++ src/main/resources/static/js/chatbot_request.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
     // sendButton 클릭 이벤트와 Enter 키 입력 이벤트 설정
     $('#sendButton').on('click', sendMessage);
-    $('#messageInput').on('keypress', function(e) {
+    $('#messageInput').on('keypress', function (e) {
         if (e.which == 13) sendMessage();
     });
 
@@ -10,23 +10,24 @@ $(document).ready(function() {
         var messageContent = $('#messageInput').val().trim();
         if (messageContent) {
             // 사용자가 입력한 메시지 출력
-            showMessage('User: ' + messageContent, 'blue');
+            showMessage(messageContent, 'user');
 
             // 서버에 보낼 메시지 형식
             var chatMessage = {
                 sender: 'User',
                 content: messageContent
             };
+
             // Ajax를 사용하여 서버에 메시지 전송
             $.ajax({
                 url: '/chatBot',  // 서버의 엔드포인트 URL
                 type: 'POST',
                 contentType: 'application/json',
                 data: JSON.stringify(chatMessage),  // JSON 형식으로 메시지 전송
-                success: function(data) {
-                    showMessage('Chatbot: ' + data, 'green');
+                success: function (data) {
+                    showMessage(data, 'bot');
                 },
-                error: function(xhr, status, error) {
+                error: function (xhr, status, error) {
                     console.error('Error:', error);
                 }
             });
@@ -35,8 +36,13 @@ $(document).ready(function() {
             $('#messageInput').val('');
         }
     }
-    // 메시지를 화면에 출력하는 함수
-    function showMessage(message, color) {
-        $('#messages').append($('<div>').text(message).css('color', color));
+
+// 메시지를 화면에 출력하는 함수
+    function showMessage(message, type) {
+        const messageClass = type === 'user' ? 'user-message' : 'bot-message';
+        $('#messages').append(
+            $('<div>').text(type === 'user' ? 'User: ' + message : 'Chatbot: ' + message)
+                .addClass(`message ${messageClass}`)
+        );
     }
 });
