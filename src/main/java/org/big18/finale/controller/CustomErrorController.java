e1b5bea7fefffa61d69c5e39ebac0f98f8ba023d
Taewoo <confort28@gmail.com>
2024-10-13 20:03:17 +0900

    에러페이지 커스텀

--- src/main/java/org/big18/finale/controller/CustomErrorController.java
+++ src/main/java/org/big18/finale/controller/CustomErrorController.java
@@ -1,12 +1,12 @@
 package org.big18.finale.controller;
 
-
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
 
 @Controller
 public class CustomErrorController implements ErrorController {
@@ -14,7 +14,13 @@ public class CustomErrorController implements ErrorController {
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request, Model model) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-        model.addAttribute("status", status);
+        // 모든 에러를 404로 처리
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error";
+            }
+        }
         return "error";
     }
 }
